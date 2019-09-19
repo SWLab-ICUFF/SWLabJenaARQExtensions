665012d2a42429a6dc13227aeff11ea6312d6f00
Luiz Leme <lapaesleme@ic.uff.br>
2019-09-19 10:03:21 -0300

    ok

--- src/main/java/uff/ic/swlab/jena/fuseki/cmd/FusekiCmd.java
+++ src/main/java/uff/ic/swlab/jena/fuseki/cmd/FusekiCmd.java
@@ -6,6 +6,7 @@ import org.apache.jena.sparql.expr.aggregate.AggCustom;
 import org.apache.jena.sparql.expr.aggregate.AggregateRegistry;
 import org.apache.jena.sparql.graph.NodeConst;
 import uff.ic.swlab.jena.sparql.aggregate.AccTMinMax;
+import uff.ic.swlab.jena.sparql.aggregate.MeanCoocurrFreq;
 
 public class FusekiCmd {
 
@@ -16,9 +17,17 @@ public class FusekiCmd {
         }
     };
 
+    private static final AccumulatorFactory tMinMaxFactory2 = new AccumulatorFactory() {
+        @Override
+        public Accumulator createAccumulator(AggCustom agg, boolean distinct) {
+            return new MeanCoocurrFreq(agg);
+        }
+    };
+
     public static void main(String[] args) {
         String aggUri = "http://uff.ic.swlab.jena.sparql.aggregate/tMinMax";
         AggregateRegistry.register(aggUri, tMinMaxFactory, NodeConst.nodeMinusOne);
+        AggregateRegistry.register(aggUri, tMinMaxFactory2, NodeConst.nodeMinusOne);
 
         org.apache.jena.fuseki.cmd.FusekiCmd.main(args);
     }
---
+++ src/main/java/uff/ic/swlab/jena/sparql/aggregate/MeanCoocurrFreq.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uff.ic.swlab.jena.sparql.aggregate;
+
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.expr.ExprEvalException;
+import org.apache.jena.sparql.expr.ExprLib;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.expr.aggregate.Accumulator;
+import org.apache.jena.sparql.expr.aggregate.AggCustom;
+import org.apache.jena.sparql.function.FunctionEnv;
+
+public class MeanCoocurrFreq implements Accumulator {
+
+    static boolean DEBUG = false;
+    protected long errorCount = 0;
+    private boolean makeDistinct = false;
+    private AggCustom agg = null;
+    private NodeValue[] minMaxSoFar = null;
+
+    public MeanCoocurrFreq(AggCustom agg) {
+        this.agg = agg;
+        this.makeDistinct = false;
+    }
+
+    @Override
+    public final void accumulate(Binding binding, FunctionEnv functionEnv) {
+        try {
+            NodeValue[] nv = {
+                ExprLib.evalOrNull(agg.getExprList().get(0), binding, functionEnv),
+                ExprLib.evalOrNull(agg.getExprList().get(1), binding, functionEnv)
+            };
+            if (nv != null) {
+                try {
+                    accumulate(nv, binding, functionEnv);
+                    return;
+                } catch (ExprEvalException ex) {
+                }
+                errorCount++;
+            }
+        } catch (ExprEvalException ex) {
+            errorCount++;
+        }
+    }
+
+    public void accumulate(NodeValue[] nv, Binding binding, FunctionEnv functionEnv) {
+        if (minMaxSoFar == null) {
+            minMaxSoFar = nv;
+            return;
+        }
+        int x = NodeValue.compareAlways(nv[1], minMaxSoFar[1]);
+        int y = NodeValue.compareAlways(nv[2], minMaxSoFar[2]);
+        if (x < 0 && y >= 0 || x == 0 && y > 0)
+            minMaxSoFar = nv;
+    }
+
+    @Override
+    public NodeValue getValue() {
+        if (errorCount == 0)
+            return minMaxSoFar[0];
+        return NodeValue.makeString("error");
+    }
+
+}
