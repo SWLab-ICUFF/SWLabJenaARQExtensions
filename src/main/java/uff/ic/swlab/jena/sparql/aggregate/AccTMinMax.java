39d7bff9dba851421548afd7c5865b803312778b
Luiz Leme <lapaesleme@ic.uff.br>
2019-09-19 13:14:07 -0300

    ok

--- src/main/java/uff/ic/swlab/jena/sparql/aggregate/AccTMinMax.java
+++ src/main/java/uff/ic/swlab/jena/sparql/aggregate/AccTMinMax.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uff.ic.swlab.jena.sparql.aggregate;
 
 import org.apache.jena.sparql.engine.binding.Binding;
